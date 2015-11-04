f22497317f7fec7f5d70327aa12ee737624f7a3c
Liqs-v2 <tobiaslindenbauer@edumail.at>
2015-11-04 14:27:30 +0100

    Remove Contact/Legals

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,9 @@
    <!--footer-->
     <div id="footer">
         <div id="footerContent">
-            <ul><a href="<?php echo $dirHelper ?>legals.php">Legals</a></ul>
-            <ul><a href="<?php echo $dirHelper ?>contact.php">Contact</a></ul>
             <ul><a href="<?php echo $dirHelper ?>sitemap.php">Sitemap</a></ul>
-            <ul id="footerRight"><a href="http://eu.battle.net/wow/en/character/aegwynn/liqz/advanced" target="_blank">WoW Armory</a></ul>
-            <ul><a href="http://www.wowprogress.com/guild/eu/aegwynn/In+flagrant%C3%AC" target="_blank">Guild</a></ul>
+            <ul id="footerRight"><a href="http://eu.battle.net/wow/en/character/shattered-hand/Liqs/advanced" target="_blank">WoW Armory</a></ul>
+            <ul><a href="http://www.wowprogress.com/guild/eu/shattered-hand/W+U+T" target="_blank">Guild</a></ul>
             <ul><a href="http://www.lolking.net/summoner/euw/24062887" target="_blank">Lolking</a></ul>
             <ul><a href="http://steamcommunity.com/id/Liqserino" target="_blank">Steam Profile</a></ul>
             <ul><a href="http://steamcommunity.com/id/Liqserino/tradeoffers/" target="_blank">Tradelink</a></ul>
@@ -13,9 +11,8 @@
         <hr>
         <div id="footerCopy">
             <ul><i class="fa fa-copyright"></i> by Tobias Lindenbauer 2015</ul>
-            <ul id="footerCopyRight"><a href="" target="_blank"><i class="fa fa-facebook fa-lg"></i></a></ul>
-            <ul><a href="" target="_blank"><i class="fa fa-google-plus fa-lg"></i></a></ul>
-            <ul><a href="" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></ul>
-            <ul><a href="" target="_blank"><i class="fa fa-youtube fa-lg"></i></a></ul>
+            <ul id="footerCopyRight"><a href="http://www.twitch.tv/mrtrunsh/profile" target="_blank"><i class="fa fa-twitch fa-lg"></i></a></ul>
+            <ul><a href="https://twitter.com/TobiLiQS" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></ul>
+            <ul><a href="https://www.youtube.com/channel/UCCzn_VY9yGtUJzltDBi4j9Q" target="_blank"><i class="fa fa-youtube fa-lg"></i></a></ul>
         </div>
     </div>
